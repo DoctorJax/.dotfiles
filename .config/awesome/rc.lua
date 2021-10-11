ddda87e4df9d8eee09769d75dc7f5669a8c0945e
DoctorJax <jdl2016nc@gmail.com>
2021-10-11 15:26:11 -0400

    Adding my emacs config that I got from DT

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -88,10 +88,10 @@ awful.layout.suit.tile.left.mirror = true
 awful.layout.layouts = {
     awful.layout.suit.fair,
     awful.layout.suit.tile,
-    --awful.layout.suit.tile.left,
+    awful.layout.suit.tile.left,
     --awful.layout.suit.tile.bottom,
     --awful.layout.suit.tile.top,
-    --awful.layout.suit.floating,
+    awful.layout.suit.floating,
     --awful.layout.suit.fair.horizontal,
     --awful.layout.suit.spiral,
     --awful.layout.suit.spiral.dwindle,
