e46523e866b921c6ed5432a46f32fd5c8acb5b72
DoctorJax <jdl2016nc@gmail.com>
2021-12-16 20:24:25 -0500

    added most of my keybindings and fixed the wallpapers

--- .scripts/wallpapers.sh
+++ .scripts/wallpapers.sh
@@ -2,29 +2,21 @@
 
 random() {
     randomwall="$(find ~/wallpapers -name '*.jpg' -type f | shuf -n 1)"
-    nitrogen --head=0 --set-zoom-fill "$randomwall"
-    nitrogen --head=1 --set-zoom-fill "$randomwall"
-    wal -n -i "$randomwall" &> /dev/null
+    feh --bg-fill -z "$randomwall"
     echo "$randomwall" > "$HOME"/.cache/randomwall
-#    echo 'awesome.restart()' | awesome-client
     printf "Random background has been chosen."
 }
 
 restore() {
     restorewall="$(cat "$HOME"/.cache/randomwall)"
-    nitrogen --head=0 --set-zoom-fill "$restorewall"
-    nitrogen --head=1 --set-zoom-fill "$restorewall"
-    wal -n -i "$restorewall" &> /dev/null
+    feh --bg-fill -z "$restorewall"
     printf "Background has been restored."
 }
 
 setwall=$2
 setbg() {
-    nitrogen --head=0 --set-zoom-fill "$setwall"
-    nitrogen --head=1 --set-zoom-fill "$setwall"
-    wal -n -i "$setwall" &> /dev/null
+    feh --bg-fill -z "$setwall"
     echo "$setwall" > "$HOME"/.cache/randomwall
-#    echo 'awesome.restart()' | awesome-client
     printf "Background has been set to $setwall."
 }
 
