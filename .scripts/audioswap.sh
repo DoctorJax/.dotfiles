35975f8ac683fcc488bc78a28c8165e7d750b9d3
DoctorJax <jdl2016nc@gmail.com>
2022-01-30 00:04:55 -0500

    cleaning up unused stuff and adding functionality

---
+++ .scripts/audioswap.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+headphones_sink="alsa_output.usb-Corsair_CORSAIR_VOID_ELITE_Wireless_Gaming_Dongle-00.analog-stereo"
+earbuds_sink="alsa_output.pci-0000_1f_00.3.analog-stereo"
+
+headphones() {
+    pactl set-default-sink $headphones_sink
+    notify-send -t 2000 -a System "Audio Swap" "Default Sink: Headphones"
+}
+
+earbuds() {
+    pactl set-default-sink $earbuds_sink
+    notify-send -t 2000 -a System "Audio Swap" "Default Sink: Earbuds"
+}
+
+togglesink() {
+    pactl get-default-sink | grep $headphones_sink && earbuds || headphones
+}
+
+
+
+recordingsink() {
+    pactl load-module module-null-sink sink_name=virtualsink
+    pactl load-module module-loopback source=virtualsink.monitor sink="$(pactl get-default-sink)"
+    notify-send -t 2000 -a System "Audio Swap" "Virtual Sink: Enabled"
+}
+
+norecordingsink() {
+    pactl unload-module module-null-sink
+    pactl unload-module module-loopback
+    notify-send -t 2000 -a System "Audio Swap" "Virtual Sink: Disabled"
+}
+
+togglerecordingsink() {
+    pactl list sinks short | grep virtualsink && norecordingsink || recordingsink
+}
+
+case "$1" in
+    -h) headphones;;
+    -e) earbuds;;
+    -t) togglesink;;
+    -r) recordingsink;;
+    -n) norecordingsink;;
+    -s) togglerecordingsink;;
+esac
