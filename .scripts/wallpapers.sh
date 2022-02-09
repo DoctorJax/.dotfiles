35c5e94455a68b68408639ed4627622ac8f90d0f
DoctorJax <jdl2016nc@gmail.com>
2022-02-09 13:18:53 -0500

    found out feh autocreates a thingy to restore wallpaper, so I cleaned up my script some

--- .scripts/wallpapers.sh
+++ .scripts/wallpapers.sh
@@ -3,20 +3,17 @@
 random() {
     randomwall="$(find ~/wallpapers -name '*.jpg' -type f | shuf -n 1)"
     feh --bg-fill -z "$randomwall"
-    echo "$randomwall" > "$HOME"/.cache/randomwall
     printf "Random background has been chosen."
 }
 
 restore() {
-    restorewall="$(cat "$HOME"/.cache/randomwall)"
-    feh --bg-fill -z "$restorewall"
+    "$HOME"/.fehbg
     printf "Background has been restored."
 }
 
 setwall=$2
 setbg() {
     feh --bg-fill -z "$setwall"
-    echo "$setwall" > "$HOME"/.cache/randomwall
     printf "Background has been set to $setwall."
 }
 
