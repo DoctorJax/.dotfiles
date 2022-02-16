29654d4c15e315055a13aa92101c8bf924a25b07
DoctorJax <jdl2016nc@gmail.com>
2022-02-16 16:11:26 -0500

    should now be able to just fix 2 things instead of like 4 files to get it working with my laptop

--- .scripts/micmute.sh
+++ .scripts/micmute.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-notify-send -i /home/jackson/Pictures/white-micicon.png -t 4000 -a System "Microphone" "$(amixer set Capture toggle | grep Mono | tail -n1)"
+notify-send -i /home/jackson/Pictures/white-micicon.png -t 4000 -a System "Microphone" "$(amixer set Capture toggle | tail -n1)"
