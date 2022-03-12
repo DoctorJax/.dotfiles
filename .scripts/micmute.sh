dd9c24adef496430999f09826b51bd899679f827
DoctorJax <jdl2016nc@gmail.com>
2022-03-12 18:45:30 -0500

    Been adding things to my streamdeck. Just got it where buttons update when my micmute is ran

--- .scripts/micmute.sh
+++ .scripts/micmute.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-notify-send -i /home/jackson/Pictures/white-micicon.png -t 4000 -a System "Microphone" "$(amixer set Capture toggle | tail -n1)"
+streamdeckmuted() {
+    sed -i "s/white-micicon.png/white-micicon-mute.png/g" ~/.streamdeck_ui.json
+    kill -USR1 `pgrep streamdeck`
+}
+
+streamdeckunmuted() {
+    sed -i "s/white-micicon-mute.png/white-micicon.png/g" ~/.streamdeck_ui.json
+    kill -USR1 `pgrep streamdeck`
+}
+
+micmute() {
+    amixer set Capture toggle
+    amixer get Capture | grep "\[on\]" && streamdeckunmuted || streamdeckmuted
+    notify-send -i /home/jackson/Pictures/white-micicon.png -t 4000 -a System "Microphone" "$(amixer get Capture | tail -n1)"
+}
+
+micmute
