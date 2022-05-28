87b88ce9484439f539c393d059dc0a8c5f3642c6
DoctorJax <jdl2016nc@gmail.com>
2022-05-28 19:42:53 -0400

    definitely didn't glitch out my mouse the first time because I forgot to set a value

--- .scripts/veryclicky.sh
+++ .scripts/veryclicky.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+delay="750"
+
+if [[ -n "$2" ]]
+then
+    delay="$2"
+fi
+
 killxdotool() {
     pkill -9 xdotool
     notify-send -t 2000 -a System "Macro" "Killed Xdotool"
 }
 
 mobgrinder() {
-    notify-send -t 2000 -a System "Macro" "Mob Grinder: Activated"
-    xdotool click --repeat 1000000 --delay 750 --window $(xdotool getactivewindow) 1
+    notify-send -t 2000 -a System "Macro" "Mob Grinder: Activated With $delay Delay"
+    xdotool click --repeat 1000000 --delay $delay --window $(xdotool getactivewindow) 1
     notify-send -t 2000 -a System "Macro" "Mob Grinder: Deactivated"
 }
 
