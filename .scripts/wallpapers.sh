cfe8dc2b3d2e938075f4cee3b2f01fce9d484035
DoctorJax <jdl2016nc@gmail.com>
2022-06-08 01:28:03 -0400

    made my wallpapers script work with wayland, as well as be able to randomly select both .jpg and .png

--- .scripts/wallpapers.sh
+++ .scripts/wallpapers.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
 random() {
-    randomwall="$(find ~/wallpapers -name '*.jpg' -type f | shuf -n 1)"
+    randomwall="$(find ~/wallpapers -name '*.[jp][pn]g' -type f | shuf -n 1)"
     feh --bg-fill -z "$randomwall"
+    pkill swaybg
+    swaybg --image "$randomwall" -m fill &
     printf "Random background has been chosen."
 }
 
 restore() {
+    restorewall=$(awk '{ print $4 }' "$HOME"/.fehbg | sed "s/'//g")
     "$HOME"/.fehbg
+    pkill swaybg
+    swaybg --image $restorewall -m fill &
     printf "Background has been restored."
 }
 
 setwall=$2
 setbg() {
     feh --bg-fill -z "$setwall"
+    pkill swaybg
+    swaybg --image "$setwall" -m fill & 
     printf "Background has been set to $setwall."
 }
 
