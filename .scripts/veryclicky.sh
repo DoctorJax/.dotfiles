128729b51252553696fcfc0a7da5cb8389184b0e
DoctorJax <jdl2016nc@gmail.com>
2022-07-22 20:35:08 -0400

    made some more macro functions

--- .scripts/veryclicky.sh
+++ .scripts/veryclicky.sh
@@ -18,7 +18,22 @@ mobgrinder() {
     notify-send -t 2000 -a System "Macro" "Mob Grinder: Deactivated"
 }
 
+rightclick() {
+    case "$3" in
+        spam)
+            notify-send -t 2000 -a System "Macro" "Rightclick: Spam"
+            xdotool click --repeat 1000000 --delay $delay --window $(xdotool getactivewindow) 3
+            notify-send -t 2000 -a System "Macro" "Rightclick: Deactivated"
+        ;;
+        hold)
+            notify-send -t 2000 -a System "Macro" "Rightclick: Hold"
+            xdotool mousedown --window $(xdotool getactivewindow) 3
+        ;;
+    esac
+}
+
 case "$1" in
     -k) killxdotool ;;
     -m) mobgrinder ;;
+    -r) rightclick "$@" ;;
 esac
