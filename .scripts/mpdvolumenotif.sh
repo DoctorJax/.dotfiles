fbaffe8cf487a7d8ae4501b093737e8a1c8e4ddc
DoctorJax <jdl2016nc@gmail.com>
2022-12-11 19:55:48 -0500

    I can now select playlists on the fly

--- .scripts/mpdvolumenotif.sh
+++ .scripts/mpdvolumenotif.sh
@@ -35,10 +35,34 @@ reloadfavorites() {
     notify-send -t 2000 -a System "MPD" "Playlist: Favorites"
 }
 
+selectplaylist() {
+    musicdir="$HOME/Music"
+    fzfmenu=($(ls $musicdir | fzf --prompt="Which Artists Would You Like : " --border=rounded --margin=5% --color=dark --height 100% --reverse --header="                    MUSIC " --info=hidden --header-first))
+    if [ -z "$fzfmenu" ]; then
+        exit 1
+    fi
+    x=1
+    while [ $x -le 1 ]
+    do
+        if [ -z "$fzfmenu" ]; then
+            x=$(( $x + 1 ))
+        fi
+        selectedartists="$selectedartists $fzfmenu"
+        fzfmenu=($(ls $musicdir | fzf --prompt="Which Artists Would You Like : " --border=rounded --margin=5% --color=dark --height 100% --reverse --header="                    MUSIC " --info=hidden --header-first))
+    done
+    mpc clear
+    mpc update
+    mpc add $selectedartists
+    mpc play
+    cd ~
+    notify-send -t 2000 -a System "MPD" "Playlist: Specifically Selected"
+}
+
 case "$1" in
     -u) up ;;
     -d) down ;;
     -p) player ;;
     -s) loadstreambeats ;;
     -f) reloadfavorites ;;
+    -sp) selectplaylist ;;
 esac
