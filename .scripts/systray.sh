1c696954c4447d099292e6e04e355f27cbf5d330
DoctorJax <jdl2016nc@gmail.com>
2022-12-27 17:05:42 -0500

    added the top bar to keybinding because yes

--- .scripts/systray.sh
+++ .scripts/systray.sh
@@ -3,10 +3,14 @@
 starttray() {
     killtray
     waybar
+    eww update bar0=false
+    eww update bar1=false
 }
 
 killtray() {
     killall waybar
+    eww update bar0=true
+    eww update bar1=true
 }
 
 toggle() {
