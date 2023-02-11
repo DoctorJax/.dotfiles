c9f48314619ada45ecbced9978f6a8452bc5ccf9
DoctorJax <jdl2016nc@gmail.com>
2023-02-11 01:24:24 -0500

    using speakers a lot now, so making it an option

--- .scripts/audioswap.sh
+++ .scripts/audioswap.sh
@@ -2,6 +2,7 @@
 
 headphones_sink="alsa_output.usb-Corsair_CORSAIR_VOID_ELITE_Wireless_Gaming_Dongle-00.analog-stereo"
 earbuds_sink="alsa_output.pci-0000_1f_00.3.analog-stereo"
+speaker_sink="alsa_output.usb-Dell_Dell_AC511_USB_SoundBar-00.analog-stereo"
 
 headphones() {
     pactl set-default-sink $headphones_sink
@@ -13,6 +14,11 @@ earbuds() {
     notify-send -t 2000 -a System "Audio Swap" "Default Sink: Earbuds"
 }
 
+speaker() {
+    pactl set-default-sink $speaker_sink
+    notify-send -t 2000 -a System "Audio Swap" "Default Sink: Speakers"
+}
+
 togglesink() {
     pactl get-default-sink | grep $headphones_sink && earbuds || headphones
 }
@@ -21,4 +27,5 @@ case "$1" in
     -h) headphones;;
     -e) earbuds;;
     -t) togglesink;;
+    -s) speaker;;
 esac
