d79bc72ed3ab247a83125bcd4087fc76ce753494
DoctorJax <jdl2016nc@gmail.com>
2023-02-25 20:17:24 -0500

    well, 4 interations later and I finally have a quick switch to empty workspace button for both monitors

--- .scripts/minimize.sh
+++ .scripts/minimize.sh
@@ -11,7 +11,34 @@ maximize() {
     sed -i '$d' "$HOME"/.cache/miniwinID
 }
 
+quickempty() {
+    focusmon=$(hyprctl monitors -j | jq -r '.[] | select(.focused == true) | .id')
+
+    NB_MONITORS=($(hyprctl monitors -j | jq -r '.[] | .id'))
+    for id in "${NB_MONITORS[@]}"; do
+        hyprctl dispatch focusmonitor "$id"
+        focusedname=$(hyprctl monitors -j | jq -r ".[] | select(.focused) .name")
+        haswindows=$(hyprctl workspaces -j | jq -r ".[] | select((.monitor == \"$focusedname\") and .windows > 0) .id" | sort | tail -1)
+
+        plusone=$((haswindows + 1))
+        hyprctl dispatch workspace "$plusone"
+
+        ydotoold &
+        sleep 0.1
+        ydotool mousemove -- 1 0
+        kill $!
+
+        if [[ "$(hyprctl activewindow -j | jq -r '.workspace.name' | awk -F ':' '{ print $1 }')" == "special" ]]; then
+            specialworkspace=$(hyprctl activewindow -j | jq -r '.workspace.name' | awk -F ':' '{ print $2 }')
+            hyprctl dispatch togglespecialworkspace "$specialworkspace"
+        fi
+    done
+
+    hyprctl dispatch focusmonitor "$focusmon"
+}
+
 case "$1" in
     -n) minimize ;;
     -m) maximize ;;
+    -q) quickempty ;;
 esac
