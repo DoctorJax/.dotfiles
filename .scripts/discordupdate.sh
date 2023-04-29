06f6af054784bd9ecca8671822c5c6de7dd88761
DoctorJax <jdl2016nc@gmail.com>
2023-04-29 12:01:01 -0400

    testing out anyrun and improved discordupdate script

--- .scripts/discordupdate.sh
+++ .scripts/discordupdate.sh
@@ -1,13 +1,27 @@
 #!/usr/bin/bash
 
-cd "$HOME"/Downloads
+update() {
+    cd "$HOME"/Downloads
 
-wget 'https://github.com/GooseMod/OpenAsar/releases/download/nightly/app.asar'
+    wget 'https://github.com/GooseMod/OpenAsar/releases/download/nightly/app.asar'
+    sudo mv app.asar /opt/discord-canary/resources/app.asar
 
-sudo mv app.asar /opt/discord-canary/resources/app.asar
+    pkill -9 DiscordCanary
+    sleep 1
+    discord-canary --enable-features=UseOzonePlatform --ozone-platform=wayland > /dev/null &
+}
 
-pkill -9 DiscordCanary
+theme() {
+    echo '{"openasar":{"setup":true,"noTyping":true,"quickstart":true,"css":"@import url(\"https://dyzean.github.io/Tokyo-Night/tokyo-night.theme.css\");"}}' > "$HOME"/.config/discordcanary/settings.json
 
-sleep 1
+    pkill -9 DiscordCanary
+    sleep 1
+    discord-canary --enable-features=UseOzonePlatform --ozone-platform=wayland > /dev/null &
+}
 
-discord-canary --enable-features=UseOzonePlatform --ozone-platform=wayland > /dev/null &
+case "$1" in
+    -u) update ;;
+    -t) theme ;;
+    -a) update; theme ;;
+    *) update ;;
+esac
