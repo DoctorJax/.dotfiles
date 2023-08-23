3bf8aebf747c452a65a00d5ffa8f7237135082dd
DoctorJax <jdl2016nc@gmail.com>
2023-08-22 23:32:10 -0400

    could be other stuff in that autostart script but eh

--- .config/bspwm/rootful/autostart.sh
+++ .config/bspwm/rootful/autostart.sh
@@ -1,5 +1,1 @@
-# Fancy Stuff
-#picom -b --experimental-backends --dbus --config /home/jackson/.config/picom/picom.conf &
 xrdb ~/.Xresources &
-
-urxvt -e "feh --no-fehbg -bg-fill /home/jackson/.config/bspwm/rootful/wallpaper.jpg" &
