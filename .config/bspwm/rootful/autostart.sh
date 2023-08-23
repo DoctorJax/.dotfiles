5aee94849e332c0b9203154d74e1e4fe7c6fcf19
DoctorJax <jdl2016nc@gmail.com>
2023-08-22 23:29:02 -0400

    Rootful windows can be useful when things don't like to work correctly

---
+++ .config/bspwm/rootful/autostart.sh
@@ -0,0 +1,5 @@
+# Fancy Stuff
+#picom -b --experimental-backends --dbus --config /home/jackson/.config/picom/picom.conf &
+xrdb ~/.Xresources &
+
+urxvt -e "feh --no-fehbg -bg-fill /home/jackson/.config/bspwm/rootful/wallpaper.jpg" &
