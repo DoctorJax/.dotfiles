7d4cb80abb20d485a6d23c35a09a7c9028e5f3df
DoctorJax <jdl2016nc@gmail.com>
2024-02-24 18:53:51 -0500

    lets see if wayland signal works

--- .config/hypr/manualstart.sh
+++ .config/hypr/manualstart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pgrep -x thunderbird > /dev/null || thunderbird &
-pgrep -x signal-desktop > /dev/null || signal-desktop &
+pgrep -x signal-desktop > /dev/null || signal-desktop --enable-features=UseOzonePlatform --ozone-platform=wayland --use-tray-icon &
 pgrep -x DiscordCanary > /dev/null || /home/jackson/.local/bin/DiscordCanary --enable-features=UseOzonePlatform --ozone-platform=wayland &
 
 pgrep -x btop > /dev/null || /home/jackson/.scripts/resources.sh &
